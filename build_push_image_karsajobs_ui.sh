ac9ca375903b22dc11b853019cd2d87220cfe68e
AnandaIlyasa <aip.ilyasa@gmail.com>
2023-04-08 01:34:12 +0700

    adding script and endpoint address in .env file

---
+++ build_push_image_karsajobs_ui.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Build docker image based on Dockerfile in current 
+# directory and name it karsajobs-ui with tag latest
+docker build . -t anandailyasa/karsajobs-ui:latest
+
+# Login to Docker Hub
+echo $PASSWORD_DOCKER_HUB | docker login -u anandailyasa --password-stdin
+
+# Upload / push local docker image to my Docker Hub repository
+docker push anandailyasa/karsajobs-ui:latest
